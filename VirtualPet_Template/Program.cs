c6495e9412805ae53a3d1f064173d4181a9306e3
rlsizer <theryanlilo@gmail.com>
2017-04-07 14:59:45 -0400

    Initial commit.

---
+++ VirtualPet_Template/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is how we instantiate a new instance of our pet
+            VirtualPet myPet = new VirtualPet();
+
+
+            //TODO add a greeting for the user. you can also let the user name the pet if you like
+
+            int selectedOption;
+
+            do
+            {
+                //TODO fill this out with more options to interact with the pet
+                Console.WriteLine();
+                Console.WriteLine("Please select an option");
+                Console.WriteLine("1. Feed the pet");
+                Console.WriteLine("10. Quit");
+
+
+                selectedOption = int.Parse(Console.ReadLine());
+
+                myPet.MyPetStatus();
+
+                switch (selectedOption)
+                {
+                    //when the user selects option one we feed the pet
+                    case 1:
+
+                        myPet.HungerDecrease();
+                        Console.WriteLine("Thank you for feeding the pet");
+                        break;
+
+                    //TODO we need to add more cases for the other ways to interact with our pet
+
+                    case 10:
+
+                        Console.WriteLine("Thank you for playing.");
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Invalid option selected.");
+                        break;
+                }
+
+                //TODO We can put method calls here so the pet can have some values change automatically
+                //Feel free to add, remove, or modify which methods are called here
+                myPet.HungerIncrease();
+
+            } while (selectedOption != 10);
+
+        }
+    }
+}
---
+++ VirtualPet_Template/VirtualPet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet_Template
+{
+
+    class VirtualPet
+    {
+        //We're setting our fields to private here so only the VirtualPet class can modify them directly
+        private string name;
+        private int hunger;
+        private int thirst;
+        private int waste;
+        private int boredom;
+        private int health;
+        private int fatigue;
+
+
+        //TODO fill in more fields
+
+        //this defaut constructor is sets options without giving us any options
+        public VirtualPet()
+        {
+            this.name = "Lee";
+            this.hunger = 20;
+            //TODO account for more fields
+        }
+
+        //TODO how can we set things with our construtor
+        public VirtualPet(string name, int hunger, int sick, int boredom)
+        {
+            this.name = name;
+            this.hunger = 0;
+            this.thirst = 0;
+            this.waste = 0;
+            this.boredom = 0;
+            this.health = 100;
+            this.fatigue = 0;
+        }
+
+        
+        //this method can be called to increase the hunger
+        public void HungerIncrease()
+        {
+            //TODO modify these values to anything you see fit
+            this.hunger = hunger + 3;
+        }
+
+        //this method can be called to decrease the hunger
+        public void HungerDecrease()
+        {   
+            //TODO modify these values to anything you see fit
+            this.hunger = hunger - 5;
+        }
+
+        //TODO we need to add more methods to modify the other fields
+
+
+        //method for stats of virtual pet
+        public void MyPetStatus()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine("Hunger: " + hunger);
+            //TODO account for more fields
+        }
+    }
+}
